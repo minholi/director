7dd227c18a3c55f5a3d21c0dac2829cd20f7eb93
Marcelo Minholi <minholi@gmail.com>
2019-07-02 17:55:32 -0300

    .gitignore fix

--- .env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\minholi\documents\code\projects\director\.env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
