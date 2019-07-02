ade4197c9c8d984a778715bccc3c9e4579021045
Marcelo Minholi <minholi@gmail.com>
2019-07-02 17:03:48 -0300

    Initial commit

---
+++ base/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=255)
+    cpf = models.CharField(max_length=14)
+
+    def __str__(self):
+        return self.nome
