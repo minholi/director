ecc912649a24534f3200f5a93b623b4c877025f6
Marcelo Minholi <minholi@gmail.com>
2019-07-10 23:13:42 -0300

    Trash cleaned

--- academico/models.py
+++ academico/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from base.models import Pessoa
 
 class Situacao(models.Model):
     id = models.IntegerField(primary_key=True)
--- base/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Pessoa(models.Model):
-    nome = models.CharField(max_length=255)
-    cpf = models.CharField(max_length=14, unique=True)
-
-    def __str__(self):
-        return self.cpf
-
